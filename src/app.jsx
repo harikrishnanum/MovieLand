3d58b4f9c9f5f3038c4a66177e2e76bf096f6d6f
hari <harikn77@gmail.com>
2023-03-12 00:56:28 +0530

    Add env file and removed unnecessary packages

--- src/app.jsx
+++ src/app.jsx
@@ -3,9 +3,7 @@ import './App.css'
 import MovieCard from './MovieCard';
 import SearchIcon from './search.svg'
 
-
-// const OMDB_API = `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}` // getting ReferenceError: process is not defined
-const OMDB_API = `http://www.omdbapi.com/?apikey=4a3b711b`
+const OMDB_API = `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_OMDB_API_KEY}`
 
 const App = () => {
   const [movies, setMovies] = useState([])
