141b633b727140fe4f49096b5b6faefca9112c33
hari <harikn77@gmail.com>
2023-03-12 01:40:56 +0530

    Add new state to manage error

--- src/app.jsx
+++ src/app.jsx
@@ -9,6 +9,7 @@ const App = () => {
   const [movies, setMovies] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
   const [loading, setLoading] = useState(false)
+  const [showEmpty, setShowEmpty] = useState(false)
 
   const searchMovies = async (title) => {
     setLoading(true)
@@ -16,6 +17,9 @@ const App = () => {
     const data = await res.json();
     setMovies(data.Search);
     setLoading(false)
+    if (!data.Search || data.Search.length === 0) {
+      setShowEmpty(true)
+    }
   }
   return (
     <>
@@ -44,11 +48,11 @@ const App = () => {
             movies?.length > 0 ?
               <div className='container'>
                 {movies.map(movie => <MovieCard movie={movie} />)}
-              </div> : (
+              </div> : showEmpty ? (
                 <div className='empty'>
                   <h2>No movies found</h2>
                 </div>
-              )
+              ) : null
         }
       </div>
     </>
