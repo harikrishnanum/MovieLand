972394a83d07902df9bc4fc924d8b5ed60883d8e
hari <harikn77@gmail.com>
2023-03-12 00:05:03 +0530

    Initial Commit

---
+++ src/app.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import './App.css'
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg'
+
+
+// const OMDB_API = `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}` // getting ReferenceError: process is not defined
+const OMDB_API = `http://www.omdbapi.com/?apikey=4a3b711b`
+
+const App = () => {
+  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const searchMovies = async (title) => {
+    setLoading(true)
+    const res = await fetch(`${OMDB_API}&s=${title}`);
+    const data = await res.json();
+    setMovies(data.Search);
+    setLoading(false)
+  }
+  return (
+    <>
+      <div className='app'>
+        <h1>MovieLand</h1>
+        <div className='search'>
+          <input
+            type="text"
+            placeholder='Search for movies...'
+            value={searchTerm}
+            onChange={(e) => { setSearchTerm(e.target.value) }}
+            onKeyDown={(e) => { e.key === 'Enter' && searchMovies(searchTerm) }}
+          />
+          <img
+            src={SearchIcon}
+            alt="Search Icon"
+            onClick={() => { searchMovies(searchTerm) }}
+          />
+        </div>
+        {
+          loading ? (
+            <dir className='empty'>
+              <h2>Loading...</h2>
+            </dir>
+          ) :
+            movies?.length > 0 ?
+              <div className='container'>
+                {movies.map(movie => <MovieCard movie={movie} />)}
+              </div> : (
+                <div className='empty'>
+                  <h2>No movies found</h2>
+                </div>
+              )
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
